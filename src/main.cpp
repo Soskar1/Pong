e8fbaa05f9ca7f3b5079db9b6a522905a0171053
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2024-09-21 12:34:26 +0300

    Added paddles as custom characters

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <LiquidCrystal_I2C.h>
 #include <Arduino.h>
 
+#include "custom_characters.h"
+
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x3F, 20, 4);
 
@@ -10,9 +12,19 @@ void setup()
 	// initialize the LCD
 	lcd.begin();
 
+  lcd.createChar(0, leftPaddleTop);
+  lcd.createChar(1, leftPaddleDown);
+  lcd.createChar(2, leftPaddle);
+
+  lcd.createChar(3, rightPaddleTop);
+  lcd.createChar(4, rightPaddleDown);
+  lcd.createChar(5, rightPaddle);
+
+  lcd.setCursor(0, 1);
+  lcd.write(2);
+
 	// Turn on the blacklight and print a message.
-	lcd.backlight();
-	lcd.print("Hello, world!");
+	// lcd.print("Hello, world!");
 }
 
 void loop()
